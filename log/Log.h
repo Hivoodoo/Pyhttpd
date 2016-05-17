022a215b7a9d9993668ce319e22ba3bdf8919552
hivoodoo <bassistdante@gmail.com>
2016-05-17 19:07:28 +0800

    add time to log

--- log/Log.h
+++ log/Log.h
@@ -2,48 +2,78 @@
 #define LOG_H
 
 #include <string>
-#include <sstream>
+#include <strstream>
 #include <iostream>
-using std::string;
-using std::cout;
-using std::endl;
+#include <ctime>
+#include <sys/time.h>	  
 
-enum class LogLevel:int{
-	MUTE=0,
-	INFO,
-	LOG,
-	VERBOSE,
-	DEBUG,
-	NUM_T};
+namespace{
+	// using std::string;
+	// using std::cout;
+	// using std::endl;
+	using namespace std;
 
-static constexpr int CODELEN= static_cast<int>(LogLevel::NUM_T);
-static const string CODESTRING[]={
-	"",
-	"INFO: ",
-	"LOG: ",
-	"VERBOSE: ",
-	"DEBUG: "};
+	enum class LogLevel:int{
+		MUTE=0,
+		INFO,
+		LOG,
+		VERBOSE,
+		DEBUG,
+		NUM_T};
 
-class Log
-{
+	static constexpr int CODELEN= static_cast<int>(LogLevel::NUM_T);
+	static const string CODESTRING[]={
+		"",
+		"INFO: ",
+		"LOG: ",
+		"VERBOSE: ",
+		"DEBUG: "};
 
-	LogLevel level;
-
-public:
-	Log(LogLevel lv=LogLevel::LOG):level(lv){}
-	
-	void i(LogLevel tag,string str)
+	class Log
 	{
-		if(tag==LogLevel::MUTE)
-			return;
-		if(static_cast<int>(tag) > static_cast<int>(level))
-			return;
-		cout<<CODESTRING[static_cast<int>(tag)]<<str<<endl;
-	}
-	void i(string tag,string str)
-	{
-		cout<<tag<<':'<<str<<endl;
-	}
-}log;
+		LogLevel level;
+		string getTime()
+		{
+			struct timeval tv;
+			gettimeofday(&tv,NULL);
+			ostrstream os;
+			time_t now;
+			now = time(NULL);//获取当前时间
+
+			struct tm *t;
+			t = localtime(&now);//转换为tm
+
+			os<<"[";
+			os<<t->tm_year+1900<<'.';
+			os<<t->tm_mon+1<<'.';
+			os<<t->tm_mday<<' ';
+			os<<t->tm_hour<<':';
+			os<<t->tm_min<<':';
+			os<<t->tm_sec<<':';
+			os<<tv.tv_usec<<"] ";
+			return os.str();
+		}
+			
+
+	public:
+		Log(LogLevel lv=LogLevel::LOG):level(lv){}
+		
+		void i(LogLevel tag,string str)
+		{
+			if(tag==LogLevel::MUTE)
+				return;
+			if(static_cast<int>(tag) > static_cast<int>(level))
+				return;
+			cout<<getTime();
+			cout<<CODESTRING[static_cast<int>(tag)];
+			cout<<str<<endl;
+		}
+		void i(string tag,string str)
+		{
+			cout<<getTime();
+			cout<<tag<<':'<<str<<endl;
+		}
+	}log;
+}
 
 #endif
